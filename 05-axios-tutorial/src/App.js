cb365e5539865440fcf8fddb4aee1c2c6b6407a8
Filip Kotev <filipkotev@yahoo.com>
2024-10-10 12:30:00 +0300

    Axios - get and post requests

--- 05-axios-tutorial/src/App.js
+++ 05-axios-tutorial/src/App.js
@@ -1,8 +1,13 @@
 import Title from './components/Title';
+import PostRequest from './examples/3-post-request';
+
+
+
 function App() {
   return (
     <main>
       <Title />
+      <PostRequest />
     </main>
   );
 }
