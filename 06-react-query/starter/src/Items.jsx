9195fc5604b52e32b5024e31dc80bb896e89c4d0
Filip Kotev <filipkotev@yahoo.com>
2024-10-11 15:57:08 +0300

    React query - first query and render the data

--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -1,8 +1,27 @@
 import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import { useQuery } from '@tanstack/react-query';
+import { customFetch } from './utils';
+
+const Items = () => {
+  const { isLoading, data, error, isError } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const { data } = await customFetch.get('/');
+      return data;
+    }
+  })
+
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem' }}>Loading...</p>
+  }
+
+  if (isError) {
+    return <p style={{marginTop: '1rem'}}>{error.message}</p>
+  }
+
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
