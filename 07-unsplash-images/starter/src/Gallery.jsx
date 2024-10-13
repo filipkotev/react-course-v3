b58ac83b6ab1674969816c774c1093db186482ff
Filip Kotev <filipkotev@yahoo.com>
2024-10-13 18:01:28 +0300

    React Query setup and fetch images

--- 07-unsplash-images/starter/src/Gallery.jsx
+++ 07-unsplash-images/starter/src/Gallery.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const VITE_UNSPLASH_KEY = import.meta.env.VITE_UNSPLASH_KEY
+const url = `https://api.unsplash.com/photos/?client_id=${VITE_UNSPLASH_KEY}&client=office`
 
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async() => {
+      const result = await axios.get(url);
+      return result.data
+    },
+  })
+console.log(response);
   return (
     <div>Gallery</div>
   )
 }
 
-export default Gallery
+export default Gallery;
--- 07-unsplash-images/starter/src/SearchForm.jsx
+++ 07-unsplash-images/starter/src/SearchForm.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
 const SearchForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+
+    if (!searchValue) {
+      return window.alert('Enter a value');
+    }
+
+    console.log(searchValue)
+    e.target.reset();
+  }
   return (
-    <div>SearchForm</div>
-  )
+    <section>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <h1 className='title'>unsplash images</h1>
+        <div className='flex'>
+          <input
+            type='text'
+            className='form-input search-input'
+            name='search'
+            placeholder='cat'
+          />
+          <button type='submit' className='btn'>search</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm
